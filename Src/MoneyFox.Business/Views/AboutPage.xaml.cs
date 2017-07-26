a7474d87ce90d8e54fd030c2dd1efa5b3dbc0c95
Nino Padrutt <nino.padrutt@hotmail.com>
2017-07-27 00:29:33 +0200

    some adjustments to display image properly

--- Src/MoneyFox.Business/Views/AboutPage.xaml.cs
+++ Src/MoneyFox.Business/Views/AboutPage.xaml.cs
@@ -1,14 +1,38 @@
-﻿using Xamarin.Forms;
+﻿using MoneyFox.Business.ViewModels;
+using MvvmCross.Platform;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MoneyFox.Business.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+	[XamlCompilation(XamlCompilationOptions.Skip)]
 	public partial class AboutPage : ContentPage
 	{
 		public AboutPage ()
 		{
 			InitializeComponent ();
-		}
+		    SetheaderImage();
+
+            BindingContext = Mvx.Resolve<AboutViewModel>();
+        }
+
+	    private void SetheaderImage()
+	    {
+	        switch (Device.RuntimePlatform)
+	        {
+	            case Device.iOS:
+	                HeaderImage.Source = "Images/applylogo.png";
+	                break;
+	            case Device.Android:
+	                HeaderImage.Source = "Images/applylogo.png";
+	                break;
+	            case Device.UWP:
+	                HeaderImage.Source = "Assets/applylogo.png";
+	                break;
+	            default:
+	                HeaderImage.Source = "applylogo.png";
+	                break;
+	        }
+        }
 	}
 }
